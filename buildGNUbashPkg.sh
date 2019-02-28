ac816a99aaa9b47c85d9f7e8e9d4ed2459b1519c
Armin Briegel <ab@scriptingosx.com>
2019-02-28 09:32:46 +0100

    can now pass version as argument, clean up and comments

--- buildGNUbashPkg.sh
+++ buildGNUbashPkg.sh
@@ -2,12 +2,32 @@
 
 export PATH=/usr/bin:/bin:/usr/sbin:/sbin
 
-# change this variable to download and build other versions
-version="5.0"
+# you can pass the version to download as argument 1
+# only tested with some versions of bash4 and bash5
+# use at your own risk with older versions
+# change the default when newer versions of bash5 are published
+version="${1:-5.0}"
+
+# set this to `1` to change the `bash` binary name to `bash4` or `bash5`
+# set this to `0` to keep the name as `bash`
+renamebinary=1
+
+# more variables
 
 pkgname="GNU-bash"
 identifier="org.gnu.bash"
 install_location="/usr/local"
+
+
+# requires Xcode or Developer Command line tools to be installs
+
+if ! xcode-select -p ; then
+    echo "this script requires Xcode or the Developer CLI tools to be installed"
+    exit 1
+fi
+
+
+# setup the directories
 
 projectdir=$(dirname "$0")
 # remove '.' from the path
@@ -23,15 +43,21 @@ if [[ ! -d "$builddir" ]]; then
     mkdir -p "$builddir"
 fi
 
+
 # clean out the build dir
 rm -Rf "${builddir}"/*
+
+
+# setup paths and names
 
 bashname="bash-${version}"
 archivename="${bashname}.tar.gz"
 archivepath="${downloaddir}/${archivename}"
 url="ftp://ftp.gnu.org/gnu/bash/${archivename}"
 
+
 # download the archive
+
 if [[ ! -f "${archivepath}" ]]; then
     echo "downloading $bashname"
     curl "$url" -o "${archivepath}"
@@ -42,7 +68,9 @@ if [[ ! -f "${archivepath}" ]]; then
     fi
 fi
 
+
 # extract the archive
+
 echo "extracting ${archivepath}"
 tar -xzvf "${archivepath}" -C "${builddir}"
 
@@ -60,16 +88,24 @@ fi
 payloaddir="${builddir}/payload"
 mkdir -p "$payloaddir"
 
+
 # configure
+
 cd "$sourcedir"
 "$sourcedir/configure" --srcdir="$sourcedir" --prefix="$payloaddir"
 
+
 # build
+
 make install
 
+
 # rename the bash binary to bash4 or bash5
-majorversion="${version:0:1}"
-mv "${payloaddir}/bin/bash" "${payloaddir}/bin/bash${majorversion}"
+
+if [[ renamebinary -eq 1 ]]; then
+    majorversion="${version:0:1}"
+    mv "${payloaddir}/bin/bash" "${payloaddir}/bin/bash${majorversion}"
+fi
 
 # build the pkg
 pkgpath="${projectdir}/${pkgname}-${version}.pkg"
