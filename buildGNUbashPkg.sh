caad31c05906aff67f329ef4fb34d4f061a49c9e
Armin Briegel <ab@scriptingosx.com>
2019-02-28 09:08:45 +0100

    archives will now be downloads in separate dir, build dir will be cleaned on every run

--- buildGNUbashPkg.sh
+++ buildGNUbashPkg.sh
@@ -2,31 +2,38 @@
 
 export PATH=/usr/bin:/bin:/usr/sbin:/sbin
 
-pkgname="GNU-bash"
+# change this variable to download and build other versions
 version="5.0"
+
+pkgname="GNU-bash"
 identifier="org.gnu.bash"
 install_location="/usr/local"
 
 projectdir=$(dirname "$0")
-
 # remove '.' from the path
 projectdir=$(python -c "import os; print(os.path.realpath('${projectdir}'))")
 
-builddir="$projectdir/build"
+downloaddir="${projectdir}/downloads"
+if [[ ! -d "$downloaddir" ]]; then
+    mkdir -p "$downloaddir"
+fi
 
+builddir="${projectdir}/build"
 if [[ ! -d "$builddir" ]]; then
     mkdir -p "$builddir"
 fi
 
+# clean out the build dir
+rm -Rf "${builddir}"/*
+
 bashname="bash-${version}"
 archivename="${bashname}.tar.gz"
-archivepath="${builddir}/${archivename}"
+archivepath="${downloaddir}/${archivename}"
 url="ftp://ftp.gnu.org/gnu/bash/${archivename}"
 
 # download the archive
-echo "downloading $bashname"
-
 if [[ ! -f "${archivepath}" ]]; then
+    echo "downloading $bashname"
     curl "$url" -o "${archivepath}"
 
     if [[ $? -ne 0 ]]; then
@@ -35,19 +42,18 @@ if [[ ! -f "${archivepath}" ]]; then
     fi
 fi
 
-# unwrap the archive
+# extract the archive
 echo "extracting ${archivepath}"
 tar -xzvf "${archivepath}" -C "${builddir}"
 
 if [[ $? -ne 0 ]]; then
-    echo "could not untar ${archivename}"
+    echo "could not extract ${archivename}"
     exit 1
 fi
 
 sourcedir="${builddir}/$bashname"
-
 if [[ ! -d "$sourcedir" ]]; then
-    echo "couldn't find $sourcedir"
+    echo "something went wrong, couldn't find $sourcedir"
     exit 1
 fi
 
